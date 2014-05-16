86397cc4bfd0c8a5f3c4c34be7c8b7ef6c8fc420
Ed J <edj@src.gnome.org>
2014-05-16 03:48:18 +0100

    Last trace of set_trace.

--- gimp-perl.h
+++ gimp-perl.h
@@ -5,13 +5,6 @@
 #ifndef EXTRA_H
 #define EXTRA_H
 
-#define TRACE_NONE	0x00
-#define TRACE_CALL	0x01
-#define TRACE_TYPE	0x11
-#define TRACE_NAME	0x21
-#define TRACE_DESC	0x41
-#define TRACE_ALL	0xff
-
 typedef char *utf8_str;
 typedef const char *utf8_str_const;
 
